9ecc68f06af2b8f765c9f2ff6199184d11b50e46
Wouter de Vos <wouter@codaisseur.com>
2017-07-31 14:11:16 +0200

    Check winner hook

--- src/services/games/games.hooks.js
+++ src/services/games/games.hooks.js
@@ -4,14 +4,16 @@ const createGame = require('../../hooks/create-game');
 
 const joinGame = require('../../hooks/join-game');
 
+const checkWinner = require('../../hooks/check-winner');
+
 module.exports = {
   before: {
     all: [ authenticate('jwt') ],
     find: [],
     get: [],
     create: [createGame()],
-    update: [joinGame()],
-    patch: [joinGame()],
+    update: [joinGame(), checkWinner()],
+    patch: [joinGame(), checkWinner()],
     remove: []
   },
 
